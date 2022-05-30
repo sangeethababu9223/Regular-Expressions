71764b954711c37ec235a4e1a7ee25b44fe126f7
Sangeetha Babu <sangeetha.babu@eqs.com>
2022-05-30 13:45:08 +0530

    split

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-let txt = "Hello world, Howdy!";
+let txt = "Jumping in Hello world, Howdy!";
 let regex1 = /Hello/;
 let regex2 = /world/;
 
 // console.log(regex1.exec(txt));
 // console.log(regex2.exec(txt));
-console.log(txt.search(regex2));
-console.log(txt.replace(regex1, 'hola'));
+console.log(txt.split(regex1));
