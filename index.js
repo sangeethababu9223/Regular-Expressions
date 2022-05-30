a0bb821c520753545ec89e0cc782888df62aac4a
Sangeetha Babu <sangeetha.babu@eqs.com>
2022-05-30 13:39:56 +0530

    match string function

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ let txt = "Hello world, Howdy!";
 let regex1 = /Hello/;
 let regex2 = /world/;
 
-console.log(regex1.exec(txt));
-console.log(regex2.exec(txt));
+// console.log(regex1.exec(txt));
+// console.log(regex2.exec(txt));
+console.log(txt.match(regex1));
