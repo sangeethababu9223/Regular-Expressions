8edf93ecd182f8babf16c15d7a5da6378dc62407
Sangeetha Babu <sangeetha.babu@eqs.com>
2022-05-30 21:15:34 +0530

    test ph no validation

--- index.js
+++ index.js
@@ -8,12 +8,20 @@
 // console.log(txt.match(regex3));
 
 
-let phoneNums = ["801-766-9754", "801-545-5454", "435-666-1212", "801-796-8010", "435-555-9801", "801-009-0909", "435-222-8013", "801-777-6655"];
-let numExp = /801-/;
-let filtArray = phoneNums.filter(value => {
-    return numExp.test(value);
-});
+// let phoneNums = ["801-766-9754", "801-545-5454", "435-666-1212", "801-796-8010", "435-555-9801", "801-009-0909", "435-222-8013", "801-777-6655"];
+// let numExp = /801-/;
+// let filtArray = phoneNums.filter(value => {
+//     return numExp.test(value);
+// });
+// console.log("Main Array");
+// console.log(phoneNums);
+// console.log("Filtered Array");
+// console.log(filtArray);
+
+let phoneNums = ["801-766-9754", "801-545-5454", "435-666-1212", "801-796-8010", "435-555-9801", "801-009-0909", "435-222-8013", "801-777-66553", "801-777-665-", "801-77A-6655", "801-778-665"];
+let numExp = /801-\d\d\d-\d\d\d\d/;
+let numberArray = phoneNums.filter(number => numExp.test(number));
 console.log("Main Array");
 console.log(phoneNums);
 console.log("Filtered Array");
-console.log(filtArray);
+console.log(numberArray);
