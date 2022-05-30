9f34e999c8fbe90f997cc7b8eca23b24b15af3f0
Sangeetha Babu <sangeetha.babu@eqs.com>
2022-05-30 22:35:43 +0530

    Update

--- Exercise3 - start/app.js
+++ Exercise3 - start/app.js
@@ -5,16 +5,19 @@ Use several different phone numbers to test.
 
 HINT: You can use the keyup event to respond to entered text. There is a CSS Class for red and green.
 */
-
-function myFunction(){
-    let PhoneField = document.getElementById('phone');
-    let phnumber = PhoneField.value;
+(function(){
+    let phoneField = document.getElementById('phone');
     let phnReg = /\(?\d{3}\)?[-.]?\d{3}[-.]?\d{4}/;
-    if(phnReg.test(phnumber)){
-        PhoneField.classList.add('green');
-        PhoneField.classList.remove('red');
-    }else{
-        PhoneField.classList.add('red');
-        PhoneField.classList.remove('green');
+    phoneField.addEventListener('keyup', checkReg)
+    function checkReg(){
+        let phnumberVal = phoneField.value;
+        if(phnReg.test(phnumberVal)){
+            phoneField.classList.add('green');
+            phoneField.classList.remove('red');
+        }else{
+            phoneField.classList.add('red');
+            phoneField.classList.remove('green');
+        }
     }
-}
+})();
+
