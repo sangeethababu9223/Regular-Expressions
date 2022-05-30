82c3ccc807a15ecbd68f6c8e4d280e9ece4e5ecf
Sangeetha Babu <sangeetha.babu@eqs.com>
2022-05-30 13:47:05 +0530

    split practical sol sample

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 let txt = "Jumping in Hello world, Howdy!";
 let regex1 = /Hello/;
 let regex2 = /world/;
+let regex3 = /\s/;
 
 // console.log(regex1.exec(txt));
 // console.log(regex2.exec(txt));
-console.log(txt.split(regex1));
+console.log(txt.split(regex3));
