2364d4693687e99e5d1bb0fe9b3e3f1de1f9e0c8
Sangeetha Babu <sangeetha.babu@eqs.com>
2022-05-31 13:08:48 +0530

    Update

--- Exercise4 - start/app.js
+++ Exercise4 - start/app.js
@@ -2,6 +2,6 @@
 The content.js file contains a string of text stored in a variable text1. This string of text is a statement that contains a day of the week as a part of the statement. Write a regular expression that will match any day of the week and then replace that day of the week in the string with Monday. Display the results to the console. 
 */
 console.log(text1);
-dayExp = /\b[mtwfs][a-z]{1,4}[nsir]day\b/gi
+dayExp = /\b(mon|tues|wednes|thurs|fri)day\b/gi
 text2 = text1.replace(dayExp,'Monday');
 console.log(text2);
