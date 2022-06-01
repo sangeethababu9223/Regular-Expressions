31a615005efd8de469adb19d898e7a7d4e023809
Nila <sangeetha9223@gmail.com>
2022-06-01 22:56:26 +0530

    update

--- Exercise5 - start/app.js
+++ Exercise5 - start/app.js
@@ -3,20 +3,19 @@ Iterate through the data provided. Use a regular expression to store the names i
 */
 
 let data = ["Jensen, Dale", "Smith, Andrea", "Jorgensen, Michael", "Vasefi, Annika", "Lopez, Monica", "Crockett, Steven"];
-let revdata = [];
 let regExp = /(\w+), (\w+)/;
 let res;
-let revItem;
 // data.forEach(function(item,index){
 //     res = regExp.exec(item);
 //     let revItem = res[2]+", "+res[1];
 //     revdata.push(revItem);
 // });
-data.map(val => {
+let revdata = data.map(val => {
     res = regExp.exec(val);
     if(res!==null){
-        revItem = res[2]+", "+res[1];
-        revdata.push(revItem);
+       return res[2]+" "+res[1];
+    }else {
+        return null;
     }
 })
 console.log(revdata);
